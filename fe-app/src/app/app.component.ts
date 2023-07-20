a0f17bb75e3653699ccc44d9cb76c5db9c25845f
Spencer <spencer@Spencers-MacBook-Pro.local>
2023-07-20 10:16:35 -0400

    Add personality

--- fe-app/src/app/app.component.ts
+++ fe-app/src/app/app.component.ts
@@ -1,13 +1,8 @@
-import { Component } from '@angular/core';
-// import * as readline from 'readline';
+import { Component, HostListener } from '@angular/core';
 import { OpenAI } from "langchain/llms/openai";
 import { BufferMemory } from 'langchain/memory';
 import { ConversationChain } from 'langchain/chains'
 import { environment } from 'src/environments'; 
-
-// import * as dotenv from "dotenv";
-// dotenv.config();
-
 
 @Component({
   selector: 'app-root',
@@ -16,6 +11,25 @@ import { environment } from 'src/environments';
 })
 export class AppComponent {
   title = 'fe-app';
+  isShowPopup: boolean = false;
+  curPersonality: string = '';
+  personalities = [
+    'A Surfer Dude',
+    'An Offensive, Insulting, Asshole',
+    'Hyper and excited',
+    'Sad',
+    'Apoplectic with rage',
+    'Surprised',
+    'Exhausted'
+  ];
+  inputText: string = '';
+  respArr: any[] = [];
+  // Practice array
+  // respArr: any[] = [
+  //   { msg: 'What is your name?', response: 'I don\'t know my name' },
+  //   { msg: 'What name would you like to have?', response: 'Walbrot Von Yonsol' },
+  //   { msg: 'Another one', response: 'Yup, here it is' }
+  // ];
 
   AIModel = new OpenAI({
     openAIApiKey: environment.OPENAI_API_KEY,
@@ -26,58 +40,48 @@ export class AppComponent {
     llm: this.AIModel,
     memory: this.memory
   });
-  inputText: string = '';
-
-
-  respArr: any[] = [];
-  // Practice array
-  // respArr: any[] = [
-  //   { msg: 'What is your name?', response: 'I don\'t know my name' },
-  //   { msg: 'What name would you like to have?', response: 'Walbrot Von Yonsol' },
-  //   { msg: 'Another one', response: 'Yup, here it is' }
-  // ];
-  
-  curPersonality: string = '';
-  personalities = [
-    'A Surfer Dude',
-    'An Offensive, Insulting, Asshole',
-    'Hyper and excited',
-    'Sad',
-    'Apoplectic with rage',
-    'Surprised',
-
-  ];
-
 
   submitForm() {
     console.log('Submitted: ', this.inputText);
     this.doLangchainStuff(this.inputText);
     this.inputText = '';
   }
-
     
   async doLangchainStuff(msg: any) {
     let msgForInput = msg;
-    // this.curPersonality = this.personalities[1]
-
     if (this.curPersonality) {
       msgForInput += ` Respond as if you are ${this.curPersonality}`;
     }
+    const tempItem = {
+      msg,
+      response: '...'
+    }
+    this.respArr.push(tempItem);
     const resp = await this.chain.call({
       input: msgForInput,
     });
-    console.log(typeof resp)
     console.log(resp);
     // this.respArr.push(resp.response);  // << Won't work
+    // Property 'response' comes from an index signature, so it must be accessed with ['response']
     const respItem = {
       msg,
       response: resp['response']
     }
+    this.respArr.pop();
     this.respArr.push(respItem);
-    // Property 'response' comes from an index signature, so it must be accessed with ['response']
   }
 
+  togglePopup() {
+    this.isShowPopup = !this.isShowPopup;
+  }
 
-  // Include ... while waiting for response
+  hidePopup() {
+    this.isShowPopup = false;
+  }
 
+  handleMenuItemClick(option: string) {
+    console.log('Selected option:', option);
+    this.curPersonality = option;
+    this.isShowPopup = false;
+  }
 }
